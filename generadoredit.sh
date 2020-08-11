a256c11a1969332406e0ce2596ed898f6277e241
VPS-ARG <67613737+VPS-ARG@users.noreply.github.com>
2020-08-10 23:25:57 -0300

    generadoredit.sh

--- generadoredit.sh
+++ generadoredit.sh
@@ -125,7 +125,7 @@ generar_key () {
 valuekey="$(date | md5sum | head -c10)"
 valuekey+="$(echo $(($RANDOM*10))|head -c 5)"
 fun_list "$valuekey"
-keyfinal=$(ofus "$IP:8888/$valuekey/$LIST")
+keyfinal=$(ofus "$IP:8448/$valuekey/$LIST")
 echo -e "KEY: $keyfinal\nGenerada!"
 echo -e "$BARRA"
 read -p "Enter para Finalizar"
