79a6e18c07726a4f64020319f8478d50af2cd749
VPS-ARG <67613737+VPS-ARG@users.noreply.github.com>
2020-08-11 09:14:08 -0300

    Update Instgerador.sh

--- Instgerador.sh
+++ Instgerador.sh
@@ -37,7 +37,7 @@ esac
 mv -f $HOME/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
-echo -e "\033[1;36m----------------------------------------------------------------\033[0m"
+echo -e "\033[1;36m---------------------------------------------------------------\033[0m"
 meu_ip () {
 MIP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 MIP2=$(wget -qO- ipv4.icanhazip.com)
@@ -46,7 +46,7 @@ echo "$IP" > /usr/bin/vendor_code
 }
 meu_ip
 echo -e "\033[1;33mInstalando Archivos... "
-echo -e "\033[1;36m----------------------------------------------------------------\033[0m"
+echo -e "\033[1;36m---------------------------------------------------------------\033[0m"
 cd $HOME
 REQUEST=$(echo $SCPresq|$SUB_DOM)
 wget -O "$HOME/lista-arq" ${REQUEST}/GERADOR > /dev/null 2>&1
@@ -72,7 +72,7 @@ service apache2 restart > /dev/null 2>&1 &
 IVAR2="/etc/key-gerador"
 echo "$Key" > $IVAR2
 rm $HOME/lista-arq
-echo -e "\033[1;36m---------------------------------------------------------------\033[0m"
+echo -e "\033[1;36m--------------------------------------------------------------\033[0m"
 echo "/usr/bin/generar.sh" > /usr/bin/generar && chmod +x /usr/bin/generar
 echo -e "\033[1;33m Perfecto, Use el Comando \033[1;31mgenerar.sh o generar \033[1;33mpara Administrar Sus Keys y
  Actualizar Base del a servidor"
