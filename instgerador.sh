3517ba5b56fd88bbd5ff6a560a5dda143e8d102f
ARGENTO <67613737+ARGENTO-VPS@users.noreply.github.com>
2020-08-17 00:13:48 -0300

    Update instgerador.sh

--- instgerador.sh
+++ instgerador.sh
@@ -14,10 +14,10 @@ case ${txt[$i]} in
 "+")txt[$i]=".";;
 "1")txt[$i]="@";;
 "@")txt[$i]="1";;
-"4")txt[$i]="?";;
-"?")txt[$i]="4";;
-"7")txt[$i]="%";;
-"%")txt[$i]="7";;
+"2")txt[$i]="?";;
+"?")txt[$i]="2";;
+"4")txt[$i]="%";;
+"%")txt[$i]="";;
 "/")txt[$i]="K";;
 "K")txt[$i]="/";;
 esac
@@ -83,5 +83,5 @@ echo -e "\033[1;33mKey Invalida!"
 echo -e "\033[1;36m-----------------------------------------------------------\033[0m"
 }
 echo -ne "\033[0m"
-echo "ScriptVpsArgK5@80@@%c36d62aeK8888:622+9?+232+@9@" > /etc/key-gerador
+echo "ScriptVpsArg/5@80@@%c36d62aeK8888:622+9?+232+@9@" > /etc/key-gerador
 apt-get install netcat -y &>/dev/null
